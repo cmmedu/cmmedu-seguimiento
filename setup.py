8a76f0a42bbd81f62366fbc58987cbe4a235956e
Vicente Daie Pinilla <vdaiep@gmail.com>
2024-11-11 19:50:15 -0300

    change setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cmmedu_seguimiento",
-    version="1.0.1",
+    version="1.0.2",
     author="Vicente Daie Pinilla",
     author_email="vdaiep@gmail.com",
     description=".",
